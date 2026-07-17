b61eef3f6d611769aa50e8e01d080c803bbe687d
jago82017 <jago82017@gmail.com>
2026-07-17 17:02:17 +0100

    Fix netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -13,9 +13,5 @@ export default defineConfig({
         assetFileNames: (asset) => asset.name?.endsWith('.css') ? 'assets/form.css' : 'assets/[name][extname]',
       },
     },
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
   },
 });
